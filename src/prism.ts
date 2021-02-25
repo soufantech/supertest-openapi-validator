ed4c2cdeb1e4a506752b2e1047447323f2cdeb82
Fábio Priamo <fhpriamo@users.noreply.github.com>
2021-02-25 19:37:16 -0300

    chore: add prism components indirection

---
+++ src/prism.ts
@@ -0,0 +1,9 @@
+export { IHttpRequest, IHttpResponse } from '@stoplight/prism-http/dist/types';
+export {
+  validateInput,
+  validateOutput,
+  validateSecurity,
+} from '@stoplight/prism-http/dist/validator';
+export { default as route } from '@stoplight/prism-http/dist/router';
+export { HttpMethod, IHttpOperation } from '@stoplight/types';
+export { IPrismDiagnostic } from '@stoplight/prism-core';
